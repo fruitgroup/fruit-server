52ae5a1eed745fa53da3232561d905bb123e59fd
ZEO <nieyujiang@gmail.com>
2017-08-17 21:37:09 +0800

    add User table init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->increments('id')->comment('Primary key');
+            $table->bigInteger('uid')->comment('User ID');
+            $table->string('user_name',50)->comment('login user name,like phone number,and wechat/QQ login tag');
+            $table->string('nick_name',50)->comment('user nick name,display on the app and admin dashboard');
+            $table->tinyInteger('login_type')->comment('1:phone number 2:email 3:wechat 4:QQ 5:more and more');
+            $table->string('password',50)->comment('login password,encrypt by md5 or aes-256-cfb');
+            $table->string('token',50)->comment('every time login automatic generate as login only dentiﬁer');
+            $table->string('verify_code',50)->comment('when user change the password use it as Security code');
+            $table->string('phone',11)->comment('phone number');
+            $table->tinyInteger('sex')->comment('sex 1: man 2: woman');
             $table->rememberToken();
             $table->timestamps();
         });
